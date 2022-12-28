01116a7f36b664ad1e482b63bcbf8558ee1bbd80
odiinnn <msinkevic103@gmail.com>
2022-12-28 20:42:37 +0300

    remove unused files

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import { ComponentMeta, ComponentStory, storiesOf } from '@storybook/react';
+import { ComponentMeta, ComponentStory } from '@storybook/react';
 
 import Button from '../components/Button';
-import { ButtonProps } from '../components/Button/Button.types';
-
-const params: ButtonProps = { children: 'Button' };
-// storiesOf('Button', module).add('Default', () => <Button variant="outlined">Button</Button>, { params });
 
 export default {
-  title: 'Example/Button',
+  title: 'Button',
   component: Button,
   parameters: {
     // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
